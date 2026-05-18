202330a62b15f2f837abb2be983ebfb2faf085a2
Subhash-2005 <subhashtalluri68@gmail.com>
2026-05-18 17:24:11 +0530

    Updated CORS for production frontend

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,9 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://tasktrackerr-nine.vercel.app/"
+}));
 app.use(express.json());
 
 let tasks = [
