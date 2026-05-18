c58eb33c5bc51bb5f33f1d8e8dfd43147ba54875
Subhash-2005 <subhashtalluri68@gmail.com>
2026-05-18 16:37:27 +0530

    Added task CRUD functionality

--- backend/server.js
+++ backend/server.js
@@ -6,10 +6,34 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.json({
-    message: "Backend Running Successfully"
-  });
+let tasks = [
+  {
+    id: 1,
+    text: "Learn CI/CD"
+  }
+];
+
+app.get("/tasks", (req, res) => {
+  res.json(tasks);
+});
+
+app.post("/tasks", (req, res) => {
+  const newTask = {
+    id: Date.now(),
+    text: req.body.text
+  };
+
+  tasks.push(newTask);
+
+  res.json(tasks);
+});
+
+app.delete("/tasks/:id", (req, res) => {
+  const id = Number(req.params.id);
+
+  tasks = tasks.filter((task) => task.id !== id);
+
+  res.json(tasks);
 });
 
 const PORT = 5000;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,73 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const API = import.meta.env.VITE_API_URL;
+
+  const [tasks, setTasks] = useState([]);
+  const [text, setText] = useState("");
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  const fetchTasks = async () => {
+    const response = await fetch(`${API}/tasks`);
+    const data = await response.json();
+    setTasks(data);
+  };
+
+  const addTask = async () => {
+    if (!text.trim()) return;
+
+    const response = await fetch(`${API}/tasks`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ text })
+    });
+
+    const data = await response.json();
+
+    setTasks(data);
+    setText("");
+  };
+
+  const deleteTask = async (id) => {
+    const response = await fetch(`${API}/tasks/${id}`, {
+      method: "DELETE"
+    });
+
+    const data = await response.json();
+    setTasks(data);
+  };
+
   return (
     <div className="container">
-      <h1>Task Tracker CI/CD Project</h1>
-      <p>Week 1 Setup Complete</p>
-      <p>Checking ci/cd by creating a new branch</p>
+      <h1>Task Tracker</h1>
+
+      <div className="input-box">
+        <input
+          type="text"
+          placeholder="Enter task"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+
+        <button onClick={addTask}>Add</button>
+      </div>
+
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            {task.text}
+
+            <button onClick={() => deleteTask(task.id)}>
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
